e601519f5bccae5bdfcf35203988c3a7f8dbd6ac
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2021-12-19 22:01:45 -0500

    mini edit

--- DB-Scripts/users.sql
+++ DB-Scripts/users.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	username VARCHAR(100) NOT NULL,
-	password VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL, 
 	email VARCHAR(100) NOT NULL,
 	user_role VARCHAR(50) NOT NULL
 );
